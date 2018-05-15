f8790c38f9c42dccbe41f51457c7b561fb8391cb
MagicToy <khan@live.co.kr>
2018-05-16 06:21:34 +0900

    Product Resource Modified

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -19,9 +19,14 @@ class ProductResource extends JsonResource
         return  [
             'name' => $this->name ,
             'description' => $this->detail, 
-            'price' => $this->price,
-            'stock' => $this->stock,
+            'tag-price' => $this->price,
+            'stock' => $this->stock  == 0 ?  'out of stock' : $this->stock ,
             'discount' => $this->discount  ,
+            'sale-price' =>  round( $this->price * ( 1 - $this->discount / 100) , 2),
+            'rating' => $this->reviews->count() > 0 ?  round($this->reviews->sum('star')  / $this->reviews->count(), 2) : ''  ,
+            'href' => [
+                'reviews' => route('reviews.index' , $this->id)
+            ],
         ];
 
     }
