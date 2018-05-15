9fcafae9209a9b785d56093bdcd92292cb070061
MagicToy <khan@live.co.kr>
2018-05-16 04:58:04 +0900

    Relationship

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -2,9 +2,13 @@
 
 namespace App\Model;
 
+use App\Model\Review;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    function reviews($value='')
+    {
+        return $this->hasMany( 'App\Model\Review' );
+    }
 }
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -2,9 +2,14 @@
 
 namespace App\Model;
 
+use App\Model\Product;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    function product($value='')
+    {
+        return $this->belongsTo('App\Model\Product' , 'product_id');
+        
+    }
 }
